070a10289f14b39213d6545d7d8792373da51848
Juan David <barbosaneira@gmail.com>
2025-09-11 12:16:05 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+  import Form from "./components/Form"
+
+  function App() {
+
+    return (
+      <>
+        <header className="bg-lime-600 py-3">
+          <div className="max-w-5xl mx-auto px-4 flex justify-between items-center">
+            <h1 className="text-4xl text-center font-bold text-white uppercase">Contador de Calorias</h1>
+          </div>
+        </header>
+
+        <section className="bg-lime-500 py-20 px-5">
+          <div className="max-w-5xl mx-auto">
+            <Form />  
+          </div>
+        </section>
+      </>
+    )
+  }
+
+  export default App
